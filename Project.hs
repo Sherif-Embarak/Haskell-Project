bca1f1f06e713210eaa38b7ce5a4bd2bcbb267db
Sherif-Embarak <sherif.embarak@gmail.com>
2016-05-01 12:15:31 +0300

    Add Sorting Algorithms

--- Project.hs
+++ Project.hs
@@ -18,8 +18,50 @@ traverseBF tree = tbf [tree]
 
 
 
+--Quick Sort 
+quicksort :: (Ord a) => [a] -> [a]
+quicksort [] = []
+quicksort (x:xs) = quicksort [y | y <- xs, y <= x] ++ [x] ++ quicksort [y | y <- xs, y > x]
+
+--Merg Sort
+mergesort'merge :: (Ord a) => [a] -> [a] -> [a]
+mergesort'merge [] xs = xs
+mergesort'merge xs [] = xs
+mergesort'merge (x:xs) (y:ys)
+    | (x < y) = x:mergesort'merge xs (y:ys)
+    | otherwise = y:mergesort'merge (x:xs) ys
+ 
+mergesort'splitinhalf :: [a] -> ([a], [a])
+mergesort'splitinhalf xs = (take n xs, drop n xs)
+    where n = (length xs) `div` 2 
+ 
+mergesort :: (Ord a) => [a] -> [a]
+mergesort xs 
+    | (length xs) > 1 = mergesort'merge (mergesort ls) (mergesort rs)
+    | otherwise = xs
+    where (ls, rs) = mergesort'splitinhalf xs
+	
+	
+
+--Bubble Sort 
+bubblesort'iter :: (Ord a) => [a] -> [a]
+bubblesort'iter (x:y:xs)
+    | x > y = y : bubblesort'iter (x:xs)
+    | otherwise = x : bubblesort'iter (y:xs)
+bubblesort'iter (x) = (x)
 
 
+bubblesort' :: (Ord a) => [a] -> Int -> [a]
+bubblesort' xs i 
+    | i == (length xs) = xs
+    | otherwise = bubblesort' (bubblesort'iter xs) (i + 1) 
+ 
+bubblesort :: (Ord a) => [a] -> [a]
+bubblesort xs = bubblesort' xs 0	
+	
+
+
+	
 
 -- Tree structure 
 -- x = Node 'A'
